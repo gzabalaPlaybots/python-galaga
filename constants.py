30b8a515a75d6287bb1462dacc2d4e5c370516e4
blWizard <mtescuela09@gmail.com>
2025-08-11 19:11:09 -0300

    Added new constants

--- constants.py
+++ constants.py
@@ -3,3 +3,5 @@ SCREEN_HEIGHT = 720
 FPS = 30
 SPRITE_SHEET = './assets/images/Insectoids.bmp'
 SPRITE_SHEET_EXPLOSION = './assets/images/Explosion.png'
+MENU_BACKGROUND = './assets/images/Main_background.png'
+GAME_OVER_BACKGROUND = './assets/images/Main_background.png'
